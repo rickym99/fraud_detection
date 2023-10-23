1089e71e24355890793f6aab27e75d6085f4d0da
rickym99 <mahapatra.ricky@gmail.com>
2023-10-23 14:43:27 +0530

    a new function sum is added

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
 
 print('adding all the data processing code related to fraud detection')
 print('happy days')
+
+a = 5
+b = 4
+    def sum(a,b):
+        c = a + b
+        return code
+
+    print('sum of two numbers')    
