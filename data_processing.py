8d23ac90858a64f9bfc74bf930e531c505b7704c
rickym99 <mahapatra.ricky@gmail.com>
2023-10-23 14:50:27 +0530

    Revert "a new function sum is added"
    This reverts commit 1089e71e24355890793f6aab27e75d6085f4d0da.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,3 @@
 
 print('adding all the data processing code related to fraud detection')
 print('happy days')
-
-a = 5
-b = 4
-    def sum(a,b):
-        c = a + b
-        return code
-
-    print('sum of two numbers')    
